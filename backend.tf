2647cc88dbb99b882a96fa9ab014690e51c752e9
Uday Thandu <udaythandu1222@gmail.com>
2026-01-05 11:19:25 +0530

    commented backend for local

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
- backend "s3" {
-   bucket           = "terraform-state-uday"
-   key              = "ci/terraform.tfstate"
-   region           = "ap-south-1"
-   dynamodb_table   = "terraform-locks"
-   encrypt          = true   
- }   
-}
+#terraform {
+# backend "s3" {
+#  bucket           = "terraform-state-uday-dev"
+#   key              = "/terraform.tfstate"
+#   region           = "ap-south-1"
+#   dynamodb_table   = "terraform-locks-dev"
+#   encrypt          = true   
+# }   
+#}
