8ace1302488cafdc70e89cd3e66e238d94e04a64
Uday Thandu <udaythandu1222@gmail.com>
2026-01-05 09:49:29 +0530

    terraform first step completed

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "project_name" {
+  description = "project name"
+  type        = string
+  default     = "terraform-ci"
+}
