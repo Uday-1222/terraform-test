60ad3cd43e626533b1760f70143dba4dd16eb89c
Uday Thandu <udaythandu1222@gmail.com>
2026-01-05 11:31:39 +0530

    terraform auth loads

--- backend.tf
+++ backend.tf
@@ -1,9 +1,12 @@
-#terraform {
-# backend "s3" {
-#  bucket           = "terraform-state-uday-dev"
-#   key              = "/terraform.tfstate"
-#   region           = "ap-south-1"
-#   dynamodb_table   = "terraform-locks-dev"
-#   encrypt          = true   
-# }   
-#}
+# PRODUCTION REMINDER
+# Enable remote backend before using this in CI or PROD
+#
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-uday-prod"
+#     key            = "ec2/terraform.tfstate"
+#     region         = "ap-south-1"
+#     dynamodb_table = "terraform-locks-prod"
+#     encrypt        = true
+#   }
+# }
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+# terraform.tfvars
+aws_region = "ap-south-1"
