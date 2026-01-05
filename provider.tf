7d42cdb33687294cfa2fc40984d43122aeea895d
Uday Thandu <udaythandu1222@gmail.com>
2026-01-05 10:24:49 +0530

    made chnges in the provider

--- provider.tf
+++ provider.tf
@@ -5,10 +5,16 @@ terraform {
       aws = {
         source = "hashicorp/aws"
         version = "~> 5.0"
-      }  
+      }
+      random = {
+        source = "hashicorp/random"
+        version = "~> 3.0"
+      }
+      
     }
  }
 
 provider "aws" {
   region = var.aws_region
 }
+
